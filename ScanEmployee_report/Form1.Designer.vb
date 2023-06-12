cbcd7aeb9d80dbeb75dd6c9a2720b1bec5bf984f
teerapongchaemchamrat <juejuechaemchamrat@gmail.com>
2023-06-12 13:57:40 +0700

    Add project files.

---
+++ ScanEmployee_report/Form1.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.EmployeeIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.EmployeeNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DepartmentDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LocationDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.EmployeeCheckInBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ApplicationDataSet1 = New ScanEmployee_report.ApplicationDataSet1()
+        Me.TestMyStoredBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ApplicationDataSet = New ScanEmployee_report.ApplicationDataSet()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TestMyStoredTableAdapter = New ScanEmployee_report.ApplicationDataSetTableAdapters.TestMyStoredTableAdapter()
+        Me.EmployeeCheckInTableAdapter = New ScanEmployee_report.ApplicationDataSet1TableAdapters.EmployeeCheckInTableAdapter()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EmployeeCheckInBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ApplicationDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TestMyStoredBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ApplicationDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.EmployeeIDDataGridViewTextBoxColumn, Me.EmployeeNameDataGridViewTextBoxColumn, Me.DepartmentDataGridViewTextBoxColumn, Me.LocationDataGridViewTextBoxColumn, Me.DateDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.EmployeeCheckInBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 81)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(585, 410)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'EmployeeIDDataGridViewTextBoxColumn
+        '
+        Me.EmployeeIDDataGridViewTextBoxColumn.DataPropertyName = "employeeID"
+        Me.EmployeeIDDataGridViewTextBoxColumn.HeaderText = "employeeID"
+        Me.EmployeeIDDataGridViewTextBoxColumn.Name = "EmployeeIDDataGridViewTextBoxColumn"
+        Me.EmployeeIDDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'EmployeeNameDataGridViewTextBoxColumn
+        '
+        Me.EmployeeNameDataGridViewTextBoxColumn.DataPropertyName = "employeeName"
+        Me.EmployeeNameDataGridViewTextBoxColumn.HeaderText = "employeeName"
+        Me.EmployeeNameDataGridViewTextBoxColumn.Name = "EmployeeNameDataGridViewTextBoxColumn"
+        Me.EmployeeNameDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'DepartmentDataGridViewTextBoxColumn
+        '
+        Me.DepartmentDataGridViewTextBoxColumn.DataPropertyName = "Department"
+        Me.DepartmentDataGridViewTextBoxColumn.HeaderText = "Department"
+        Me.DepartmentDataGridViewTextBoxColumn.Name = "DepartmentDataGridViewTextBoxColumn"
+        Me.DepartmentDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'LocationDataGridViewTextBoxColumn
+        '
+        Me.LocationDataGridViewTextBoxColumn.DataPropertyName = "Location"
+        Me.LocationDataGridViewTextBoxColumn.HeaderText = "Location"
+        Me.LocationDataGridViewTextBoxColumn.Name = "LocationDataGridViewTextBoxColumn"
+        Me.LocationDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'DateDataGridViewTextBoxColumn
+        '
+        Me.DateDataGridViewTextBoxColumn.DataPropertyName = "Date"
+        Me.DateDataGridViewTextBoxColumn.HeaderText = "Date"
+        Me.DateDataGridViewTextBoxColumn.Name = "DateDataGridViewTextBoxColumn"
+        Me.DateDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'EmployeeCheckInBindingSource
+        '
+        Me.EmployeeCheckInBindingSource.DataMember = "EmployeeCheckIn"
+        Me.EmployeeCheckInBindingSource.DataSource = Me.ApplicationDataSet1
+        '
+        'ApplicationDataSet1
+        '
+        Me.ApplicationDataSet1.DataSetName = "ApplicationDataSet1"
+        Me.ApplicationDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'TestMyStoredBindingSource
+        '
+        Me.TestMyStoredBindingSource.DataMember = "TestMyStored"
+        Me.TestMyStoredBindingSource.DataSource = Me.ApplicationDataSet
+        '
+        'ApplicationDataSet
+        '
+        Me.ApplicationDataSet.DataSetName = "ApplicationDataSet"
+        Me.ApplicationDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Mali", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(176, -6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(245, 61)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Database Data"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Mali", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(907, -6)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(203, 68)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Bplus Data"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 57)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "วันที่ (ปป-ดด-วว)"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1222, 52)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Load Data"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 497)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(82, 34)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Export Excel"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(101, 54)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(91, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TestMyStoredTableAdapter
+        '
+        Me.TestMyStoredTableAdapter.ClearBeforeFill = True
+        '
+        'EmployeeCheckInTableAdapter
+        '
+        Me.EmployeeCheckInTableAdapter.ClearBeforeFill = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(212, 52)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Search"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.AllowUserToAddRows = False
+        Me.DataGridView2.AllowUserToDeleteRows = False
+        Me.DataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(639, 81)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.ReadOnly = True
+        Me.DataGridView2.Size = New System.Drawing.Size(683, 410)
+        Me.DataGridView2.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(639, 497)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 34)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Export Excel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(1236, 497)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(86, 34)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Update Excel "
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1353, 543)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Employee Scan Program"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EmployeeCheckInBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ApplicationDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TestMyStoredBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ApplicationDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TestMyStoredBindingSource As BindingSource
+    Friend WithEvents ApplicationDataSet As ApplicationDataSet
+    Friend WithEvents TestMyStoredTableAdapter As ApplicationDataSetTableAdapters.TestMyStoredTableAdapter
+    Friend WithEvents ApplicationDataSet1 As ApplicationDataSet1
+    Friend WithEvents EmployeeCheckInBindingSource As BindingSource
+    Friend WithEvents EmployeeCheckInTableAdapter As ApplicationDataSet1TableAdapters.EmployeeCheckInTableAdapter
+    Friend WithEvents Button4 As Button
+    Friend WithEvents EmployeeIDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents EmployeeNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DepartmentDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents LocationDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DateDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ ScanEmployee_report/Form1.vb
@@ -0,0 +1,281 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Net
+Imports HtmlAgilityPack
+Imports OfficeOpenXml
+Imports System.Text
+
+
+Public Class Form1
+
+    Dim connection As New SqlConnection("Data Source=192.168.10.114\APPSERVER;Initial Catalog=Application;User ID=sa;Password=Cyf027065055")
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to exit?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If result = DialogResult.No Then
+            e.Cancel = True ' Cancel the closing event
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'System.Diagnostics.Process.Start("http://ad00/infor/working.aspx")
+        DataGridView2.VirtualMode = True
+        AddHandler DataGridView2.CellValueNeeded, AddressOf DataGridView2_CellValueNeeded
+        PopulateDataTable()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'ApplicationDataSet1.EmployeeCheckIn' table. You can move, or remove it, as needed.
+        Me.EmployeeCheckInTableAdapter.Fill(Me.ApplicationDataSet1.EmployeeCheckIn)
+        FilterData("")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial
+        ' Create a new Excel package
+        Using package As New ExcelPackage()
+            ' Add a new worksheet to the Excel package
+            Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets.Add("Data")
+
+            ' Get the filtered rows from the DataGridView and export to Excel
+            Dim filteredRows As DataGridViewRowCollection = DataGridView1.Rows
+
+            ' Set the column headers in Excel
+            For columnIndex As Integer = 0 To DataGridView1.Columns.Count - 1
+                worksheet.Cells(1, columnIndex + 1).Value = DataGridView1.Columns(columnIndex).HeaderText
+            Next
+
+            ' Set the row data in Excel
+            For rowIndex As Integer = 0 To filteredRows.Count - 1
+                For columnIndex As Integer = 0 To DataGridView1.Columns.Count - 1
+                    If columnIndex = 4 Then ' Column 5 (zero-based index)
+                        Dim originalValue As Date = CDate(filteredRows(rowIndex).Cells(columnIndex).Value)
+                        Dim formattedValue As String = originalValue.ToString("dd/MM/yy HH:mm:ss")
+                        worksheet.Cells(rowIndex + 2, columnIndex + 1).Value = formattedValue
+                    Else
+                        worksheet.Cells(rowIndex + 2, columnIndex + 1).Value = filteredRows(rowIndex).Cells(columnIndex).Value
+                    End If
+                Next
+            Next
+
+            ' Save the Excel package to a file
+            Dim saveFileDialog As New SaveFileDialog()
+            saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx"
+            saveFileDialog.FileName = "SQLData " & TextBox1.Text & ".xlsx"
+
+            If saveFileDialog.ShowDialog() = DialogResult.OK Then
+                Dim filePath As String = saveFileDialog.FileName
+                Dim file As New FileInfo(filePath)
+                package.SaveAs(file)
+                MessageBox.Show("Data exported to Excel successfully.")
+            End If
+        End Using
+    End Sub
+
+    Public Sub FilterData(valueToSearch As String)
+
+
+        Dim searchQuery As String = "SELECT * FROM EmployeeCheckIn WHERE Date between '" & valueToSearch & " 00:00:00' and '" & valueToSearch & " 23:59:59'"
+
+        Dim command As New SqlCommand(searchQuery, connection)
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        FilterData(TextBox1.Text)
+    End Sub
+
+    Private dataTable As New DataTable()
+
+    Private Sub PopulateDataTable()
+        ' Specify the URL of the webpage to retrieve data from
+        Dim url As String = "http://ad00/infor/working.aspx"
+
+        ' Create a WebClient instance to download the HTML content
+        Dim client As New WebClient()
+        client.Encoding = Encoding.UTF8
+        Dim htmlContent As String = client.DownloadString(url)
+
+        ' Load the HTML content into an HtmlDocument
+        Dim htmlDocument As New HtmlAgilityPack.HtmlDocument()
+        htmlDocument.LoadHtml(htmlContent)
+
+        ' Select all <tr> elements within the table
+        Dim trNodes As HtmlNodeCollection = htmlDocument.DocumentNode.SelectNodes("//table//tr")
+
+        ' Add columns to the DataTable with desired column names
+        dataTable.Columns.Add("No")
+        dataTable.Columns.Add("Date")
+        dataTable.Columns.Add("Parent_code")
+        dataTable.Columns.Add("Parent_desc")
+        dataTable.Columns.Add("Child_code")
+        dataTable.Columns.Add("Child_desc")
+        dataTable.Columns.Add("SSN")
+        dataTable.Columns.Add("Title")
+        dataTable.Columns.Add("Name")
+        dataTable.Columns.Add("Surname")
+        dataTable.Columns.Add("Phone")
+        dataTable.Columns.Add("SF_code")
+        dataTable.Columns.Add("SF_desc")
+        dataTable.Columns.Add("Stamp_1")
+        dataTable.Columns.Add("Stamp_2")
+        dataTable.Columns.Add("Stamp_3")
+        dataTable.Columns.Add("Stamp_4")
+
+        ' Iterate over the <tr> elements and extract the cell values
+        For rowIndex As Integer = 1 To trNodes.Count - 1 ' Start from index 1 to skip the header row
+            ' Select all <td> elements within the current <tr>
+            Dim tdNodes As HtmlNodeCollection = trNodes(rowIndex).SelectNodes("td")
+
+            ' Check if the number of <td> elements matches the number of columns
+            If tdNodes IsNot Nothing AndAlso tdNodes.Count = dataTable.Columns.Count Then
+                ' Create a new row in the DataTable
+                Dim row As DataRow = dataTable.NewRow()
+
+                ' Extract the cell values and add them to the DataTable
+                For columnIndex As Integer = 0 To tdNodes.Count - 1
+                    row(columnIndex) = tdNodes(columnIndex).InnerText
+                Next
+
+                ' Add the row to the DataTable
+                dataTable.Rows.Add(row)
+            End If
+        Next
+
+        ' Set the DataTable as the data source for the DataGridView
+        DataGridView2.DataSource = dataTable
+    End Sub
+
+    Private Sub DataGridView2_CellValueNeeded(sender As Object, e As DataGridViewCellValueEventArgs)
+        ' Check if the requested column index is valid
+        If e.ColumnIndex >= 0 AndAlso e.ColumnIndex < dataTable.Columns.Count Then
+            ' Check if the requested row index is valid
+            If e.RowIndex >= 0 AndAlso e.RowIndex < dataTable.Rows.Count Then
+                ' Retrieve the value for the specified cell in the DataTable
+                e.Value = dataTable.Rows(e.RowIndex)(e.ColumnIndex)
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        ' Set the license context for EPPlus
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial
+
+        ' Create a new Excel package
+        Using package As New ExcelPackage()
+            ' Add a new worksheet to the Excel package
+            Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets.Add("Data")
+
+            ' Get the filtered rows from the DataGridView and export to Excel
+            Dim filteredRows As DataGridViewRowCollection = DataGridView2.Rows
+
+            ' Set the column headers in Excel
+            For columnIndex As Integer = 0 To DataGridView2.Columns.Count - 1
+                worksheet.Cells(1, columnIndex + 1).Value = DataGridView2.Columns(columnIndex).HeaderText
+            Next
+
+            ' Set the row data in Excel
+            For rowIndex As Integer = 0 To filteredRows.Count - 1
+                For columnIndex As Integer = 0 To DataGridView2.Columns.Count - 1
+                    worksheet.Cells(rowIndex + 2, columnIndex + 1).Value = filteredRows(rowIndex).Cells(columnIndex).Value
+                Next
+            Next
+
+            ' Save the Excel package to a file
+            Dim saveFileDialog As New SaveFileDialog()
+            saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx"
+            Dim dt As Date = Date.Today
+            saveFileDialog.FileName = "BplusData " & dt.ToString("dd-MM-yyyy") & ".xlsx"
+
+            If saveFileDialog.ShowDialog() = DialogResult.OK Then
+                Dim filePath As String = saveFileDialog.FileName
+                Dim file As New FileInfo(filePath)
+                package.SaveAs(file)
+                MessageBox.Show("Data exported to Excel successfully.")
+            End If
+        End Using
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' Load the Excel file using EPPlus
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial
+        Using package As New ExcelPackage(New FileInfo("D:\Report ScanEmployee\SCAN2023 QR-Bplus.xlsx"))
+            ' Get the desired worksheet by name
+            Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets("BPLUS4001")
+
+            ' Check if the worksheet exists
+            If worksheet IsNot Nothing Then
+                ' Clear the existing data in the worksheet
+                worksheet.Cells.Clear()
+
+                ' Set the column headers in the first row of the worksheet
+                For columnIndex As Integer = 0 To DataGridView2.Columns.Count - 1
+                    worksheet.Cells(1, columnIndex + 1).Value = DataGridView2.Columns(columnIndex).HeaderText
+                Next
+
+                ' Get the DataTable underlying the DataGridView
+                Dim dataTable As DataTable = DirectCast(DataGridView2.DataSource, DataTable)
+
+                ' Copy the data from the DataTable to the worksheet
+                For rowIndex As Integer = 0 To dataTable.Rows.Count - 1
+                    For columnIndex As Integer = 0 To dataTable.Columns.Count - 1
+                        worksheet.Cells(rowIndex + 2, columnIndex + 1).Value = dataTable.Rows(rowIndex)(columnIndex)
+                    Next
+                Next
+
+                ' Save the changes in the Excel file
+                package.Save()
+
+            Else
+                ' Handle the case when the worksheet is not found
+                MessageBox.Show("Worksheet 'BPLUS4001' not found.")
+            End If
+        End Using
+
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial
+        Using package As New ExcelPackage(New FileInfo("D:\Report ScanEmployee\SCAN2023 QR-Bplus.xlsx"))
+            ' Get the desired worksheet by name
+            Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets("SQL(QR)3001")
+
+            ' Check if the worksheet exists
+            If worksheet IsNot Nothing Then
+                ' Clear the existing data in the worksheet
+                worksheet.Cells.Clear()
+
+                ' Set the column headers in the first row of the worksheet
+                For columnIndex As Integer = 0 To DataGridView1.Columns.Count - 1
+                    worksheet.Cells(1, columnIndex + 1).Value = DataGridView1.Columns(columnIndex).HeaderText
+                Next
+
+                ' Get the DataTable underlying the DataGridView
+                Dim dataTable As DataTable = DirectCast(DataGridView1.DataSource, DataTable)
+
+                ' Copy the data from the DataTable to the worksheet
+                For rowIndex As Integer = 0 To dataTable.Rows.Count - 1
+                    For columnIndex As Integer = 0 To dataTable.Columns.Count - 1
+                        worksheet.Cells(rowIndex + 2, columnIndex + 1).Value = dataTable.Rows(rowIndex)(columnIndex)
+                    Next
+                Next
+
+                ' Save the changes in the Excel file
+                package.Save()
+                MessageBox.Show("Worksheet 'BPLUS4001 & SQL(QR)3001' Success")
+            Else
+                ' Handle the case when the worksheet is not found
+                MessageBox.Show("Worksheet 'SQL(QR)3001' not found.")
+            End If
+        End Using
+    End Sub
+
+End Class
